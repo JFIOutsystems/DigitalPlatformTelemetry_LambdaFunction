ba8061710eed832ae79c01efa190a8414576e4c8
João Figueiredo <joao.figueiredo@outsystems.com>
2019-02-18 17:50:05 +0000

    Changed the name of the project

---
+++ DigitalPlatformTelemetry/Function.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using System.Text;
+using System.IO;
+using Amazon.KinesisFirehose;
+using Amazon.KinesisFirehose.Model;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace DigitalPlatformTelemetry
+{
+
+    public class TelemetryList
+    {
+        public IList<Telemetry> Telemetry { get; set; }
+
+    }
+
+    public class Function
+    {
+
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            APIGatewayProxyResponse APIResponse = new APIGatewayProxyResponse();
+            try
+            {
+                bool EVSendToFirehose = Environment.GetEnvironmentVariable("PUSH_TO_FIREHOSE").ToUpper() == "TRUE";
+
+               
+                    if ( EVSendToFirehose) // Only Processes if SentToFirehose==True
+                    {
+                        /*Json to Telemetry List*/
+                        TelemetryList telemetryList = JsonConvert.DeserializeObject<TelemetryList>(request.Body);
+                        int ItemCount = telemetryList.Telemetry.Count;
+
+                        /*Get Environment Vars*/
+                        string EVDeliveryStream = Environment.GetEnvironmentVariable("FIREHOSE_DELIVERYSTREAM");
+                        string EVRegion = Environment.GetEnvironmentVariable("REGION");
+                        byte[] oByte;
+
+                        Amazon.RegionEndpoint Region= Amazon.RegionEndpoint.GetBySystemName(EVRegion);
+
+                        foreach(Telemetry telemetry in telemetryList.Telemetry) {
+                            telemetry.Instant = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+                            
+                            oByte = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(telemetry));
+                            using (MemoryStream ms = new MemoryStream(oByte))
+                            {
+                                Record RecordToSend = new Record
+                                {
+                                    Data = ms
+                                };
+
+                                PutRecordRequest DeliveryRequest = new PutRecordRequest
+                                {
+                                    DeliveryStreamName = EVDeliveryStream, //DigitalPlatformTelemetry
+                                    Record = RecordToSend
+                                };
+
+                                AmazonKinesisFirehoseClient FirehoseClient = new AmazonKinesisFirehoseClient(Region);
+
+                                PutRecordResponse Response = FirehoseClient.PutRecord(DeliveryRequest);
+
+                            }
+
+                        }
+
+                        APIResponse.StatusCode = 200;
+                        APIResponse.Body = "Sucess, processed " + ItemCount + " items " + DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+                        return APIResponse;
+
+                    }
+                    APIResponse.StatusCode = 200;
+                    APIResponse.Body = APIResponse.Body = "Sucess, no entries processed ( PUSH_TO_FIREHOSE = False ). " + DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+                    return APIResponse;
+              
+            }
+            catch (InvalidCastException e)
+            {
+                
+                APIResponse.StatusCode = 500;
+                APIResponse.Body = e.Message;
+                return APIResponse;
+            }
+        }
+    }
+}
---
+++ DigitalPlatformTelemetry/Telemetry.cs
@@ -0,0 +1,32 @@
+﻿
+using Newtonsoft.Json;
+
+namespace DigitalPlatformTelemetry
+{
+    public class Telemetry
+    {
+        public Telemetry(string id, string version, string logtype, string appSource, string instant, string payload)
+        {
+            this.Id = id;
+            this.Version = version;
+            this.LogType = logtype;
+            this.AppSource = appSource;
+            this.Instant = instant;
+            this.Payload = DeserializePayload(payload);
+        }
+
+        public string Id { get; set; }
+        public string Version { get; set; }
+        public string LogType { get; set; }
+        public string AppSource { get; set; }
+        public string Instant { get; set; }
+        public object Payload { get; set; }
+
+        public object DeserializePayload(string payload)
+        {
+
+            try { return JsonConvert.DeserializeObject<object>(payload); }
+            catch { return payload; }
+        }
+    }
+}
